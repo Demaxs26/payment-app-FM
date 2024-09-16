87f9a6bb58755d419c94846f56769444ddd88544
Maxime Vernay <vernaymaxime4@gmail.com>
2024-09-16 20:39:22 +0200

    js calcul and last display

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const btncalcul = document.querySelector(".calculate-button");
 const emptyRightSide = document.querySelector(".Empty-resultat-card");
 const calculatedRigthSide = document.querySelector(".Complete-result-card");
 const errMessageList = document.querySelectorAll(".err-message");
+const BigPrice = document.querySelector(".yellow-text");
+const littlePrice = document.querySelector(".little-result-box h1");
 
 function f_Switch_EmptyCalculed_RightSide(displayEmpty,displayCalculated){ // param : none if we want to remove / flex if we want to add
     emptyRightSide.style.display = displayEmpty;
@@ -13,12 +15,9 @@ function f_Switch_EmptyCalculed_RightSide(displayEmpty,displayCalculated){ // pa
 }
 
 function f_Calculate_Repayments(Amount,Term,Rate){
-    let repayments = 0;
-    for(let i = 0; i<Term;i++){  // for each years calculate the profit in function of the basic amount
-        repayments += Amount*Rate;  // more the sum of the last interest
-        Amount+= Amount*Rate;
-    }
-    return repayments
+    let repaymentsPerMonth =(Amount*(Rate/12))/(1-(1/(1+(Rate/12))**(12*Term))); // mortage calcul explained here :
+                                               //https://www.youtube.com/watch?v=_WWVxFSHCKs
+    return repaymentsPerMonth
 }
 
 function f_Repayments(){
@@ -26,18 +25,28 @@ function f_Repayments(){
     const Term = mortageterm.value;
     let Rate = mortageRate.value*(10**-2);  // convert in percentage with can be multiply
     let repayment = 0;
+    const repaymentsPerMonth =f_Calculate_Repayments(Amount,Term,Rate);
     if (mortageTypeList[0].checked){  // check the type of Mortage 
-        repayment = Amount+f_Calculate_Repayments(Amount,Term,Rate);
+        repayment = repaymentsPerMonth*12*Term; // Total repayment
+        
     }else{
-        repayment = f_Calculate_Repayments(Amount,Term,Rate);
+        repayment = repaymentsPerMonth*12*Term-Amount; // only added bby rate repayment
     }
-    return repayment
+    return [repayment ,repaymentsPerMonth]
 }
 
 function f_Add_Result_Page(repayment){
-    
+    BigPrice.textContent = "£"+repayment[0];
+    littlePrice.textContent = "£"+repayment[1];
 }
 
+
+function f_displayCorectPrice(price){
+    if (price.includes(".")){
+        const index = price.find((element)=> element == ".");
+        
+    }
+}
 
 btncalcul.addEventListener("click", function(){  // when user click on the btn
     const repayment = f_Repayments();                    // first calculate the reayment 
